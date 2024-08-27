106efb9a3ad89a00ccc37a93745223319dc4c2ed
Andrii <andrii@Andriis-Laptop.local>
2024-08-27 13:14:05 +0300

    Initial commit

---
+++ src/main/java/com/martishyn/jobsapi/JobsRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.martishyn.jobsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martishyn/jobsapi/JobsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martishyn.jobsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
